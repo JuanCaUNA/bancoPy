8c571e8854de4a97819db6103115b8a3ad59c49c
JuanCaUNA <juan.camacho.solano@est.una.ac.cr>
2025-06-09 14:32:22 -0600

    check-point

--- pythonProject/app/__init__.py
+++ pythonProject/app/__init__.py
@@ -4,7 +4,6 @@ SINPE Banking System Flask Application Factory
 
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy  # noqa: F401
 from app.models import db
 import os
 
